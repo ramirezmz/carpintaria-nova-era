3314f36c0c3795583d41281b3afc5d8e2251a39f
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-29 09:50:08 -0300

    feat: import services

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 import About from './pages/About'
+import Services from './pages/Services'
 
 function App() {
    return (
@@ -9,6 +10,7 @@ function App() {
          <Header />
          <Banner />
          <About />
+         <Services />
       </div>
    )
 }
