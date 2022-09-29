592a539aedf3054df1d51573d091b707e4b9e9b3
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-29 09:15:27 -0300

    feat: add about page + styled

---
+++ src/pages/styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const AboutContent = styled.section`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   padding: 3.2rem;
+   color: #fff;
+`
+
+export const Title = styled.h1`
+   font-weight: 900;
+`
+
+export const Paragraph = styled.p`
+   padding: 0 13rem;
+   margin-top: 2rem;
+   line-height: 2rem;
+   text-align: center;
+`
