a56e50a4aa6ba7bd3b20059789f58a32c8ea9d2c
amandadeog <amandadeog@gmail.com>
2022-10-04 00:42:51 -0300

    resolve responsive page

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,11 +1,12 @@
-import styled from 'styled-components'
-import { Navicon } from '@styled-icons/evil/Navicon'
+import styled from "styled-components";
+import { Navicon } from "@styled-icons/evil/Navicon";
 
 export const HeaderContent = styled.header`
    display: flex;
    align-items: center;
    justify-content: end;
-`
+`;
+
 export const List = styled.ul`
    padding: 1.5rem;
    font-size: 1.2rem;
@@ -14,15 +15,15 @@ export const List = styled.ul`
       margin-left: 3rem;
       text-decoration: none;
    }
-   a:hover{
-      color: #F7AE53;
+   a:hover {
+      color: #f7ae53;
       transition: 0.3s;
    }
-`
+`;
 
 export const NavIcon = styled(Navicon)`
    color: #fff;
    width: 2.4rem;
    padding: 1.5rem;
    cursor: pointer;
-`
+`;
