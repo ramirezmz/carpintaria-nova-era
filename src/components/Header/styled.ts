a4183e2533debcd9ce012bf7ed543d3b3e85f303
amandadeog <amandadeog@gmail.com>
2022-10-04 19:00:18 -0300

    fix: removendo

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,11 +1,11 @@
-import styled from "styled-components";
-import { Navicon } from "@styled-icons/evil/Navicon";
+import styled from "styled-components"
+import { Navicon } from "@styled-icons/evil/Navicon"
 
 export const HeaderContent = styled.header`
    display: flex;
    align-items: center;
    justify-content: end;
-`;
+`
 
 export const List = styled.ul`
    padding: 1.5rem;
@@ -19,11 +19,11 @@ export const List = styled.ul`
       color: #f7ae53;
       transition: 0.3s;
    }
-`;
+`
 
 export const NavIcon = styled(Navicon)`
    color: #fff;
    width: 2.4rem;
    padding: 1.5rem;
    cursor: pointer;
-`;
+   `
