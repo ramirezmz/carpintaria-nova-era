f00be8dbfa7883de932f69a34fca3fb54b387e9f
amandadeog <amandadeog@gmail.com>
2022-10-04 17:56:27 -0300

    feat: Estilo do campo de input e label

--- src/components/Field/styled.ts
+++ src/components/Field/styled.ts
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 export const ContentField = styled.div`
-   padding: 1rem;
+   padding: 2rem 2rem 1rem;
+   width: 60vw;
    input {
-      display: block;
+      background-color: #fff;
+      border-radius: 5px;
       border: none;
-      padding: 10px;
+      display: block;
+      margin: .5rem 0;
+      padding: 1rem;
       width: 100%;
+   }
+   label {
+      font-size: 1.5rem;
    }
 `;
