002a251f18a4a9cf0e8e4b313e7bc62c4095c9c0
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-11 01:29:33 -0300

    fix: add url

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,8 +4,8 @@ import {BannerComponent} from './styled'
 function Banner() {
    return (
       <picture>
-         <source media="(max-width:700px)" srcSet="/public/banner-tablet.png"/>
-         <BannerComponent src="/public/banner.png" alt='banner carpintaria nova era'/>
+         <source media="(max-width:700px)" srcSet="https://i.ibb.co/SXksVXY/banner-tablet.png"/>
+         <BannerComponent src="https://i.ibb.co/Jz6f8xf/banner.png" alt='banner carpintaria nova era'/>
       </picture>
    )
 }
--- src/components/Banner/styled.ts
+++ src/components/Banner/styled.ts
@@ -2,7 +2,4 @@ import styled from 'styled-components'
 
 export const BannerComponent = styled.img`
    width: 100vw;
-
-   @media only screen and (max-width: 700px) {
-      src: url('/public/banner-phone.png');
-   }`
+`
