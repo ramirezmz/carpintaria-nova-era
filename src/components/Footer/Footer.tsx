466babd9bc955af572e9d2b453863cd7c283f161
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-11 00:55:46 -0300

    feat: create footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FooterContent, InfoContent } from './styled'
+
+function Footer() {
+   return (
+      <FooterContent>
+         <img src="/public/footer.svg" alt="Carpintaria logo" />
+         <InfoContent>
+            <h3>CNPJ: 45361976000128</h3>
+            <span>Telefone: (22) 99925-7134</span>
+            <span>Macaé RJ</span>
+            <span>Todos os direitos reservados</span>
+         </InfoContent>
+      </FooterContent>
+   )
+}
+
+export default Footer
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const FooterContent = styled.footer`
+   display: flex;
+   align-items: center;
+   justify-content: space-around;
+
+   img {
+      padding: 2rem;
+      width: 23rem;
+   }
+`
+
+export const InfoContent = styled.div`
+   color: #fff;
+   display: flex;
+   flex-direction: column;
+   text-align: center;
+`
