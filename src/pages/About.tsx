72b0c7f56d9021e8981b76f53c7660171d8c1135
amandadeog <amandadeog@gmail.com>
2022-10-25 12:06:08 -0300

    feat: add about business

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,9 @@ function About() {
    return (
       <Content>
          <Title>Sobre a empresa</Title>
-         <Paragraph>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Paragraph>
+         <Paragraph>
+            Somos uma empresa especializada no ramo da carpintaria, situada na cidade de Macaé, norte do estado do Rio de Janeiro. A Carpintaria Nova Era, foi criada em 2018, por Horácio Andrade, carpinteiro com 16 anos de profissão. Hoje, somos uma equipe composta por 5 pessoas, que trabalham juntas para oferecer o melhor serviço aos nossos clientes, e atendemos a toda a região norte e região dos lagos fluminense.
+         </Paragraph>
          <Title>Valores</Title>
          <ContentValore>
             { values.map(({title, description, id}) => <Value title={title} description={description} key={id}/>) }
